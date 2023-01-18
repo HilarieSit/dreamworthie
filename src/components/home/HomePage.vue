d7f14bb403825f5c2654bc5fb041d2bba3900657
Hilarie Sit <hs764@cornell.edu>
2023-01-17 21:30:28 -0500

    add three js model

--- src/components/home/HomePage.vue
+++ src/components/home/HomePage.vue
@@ -19,11 +19,11 @@
         <h3 data-aos="fade-up"> Accessible 3D Models </h3>
         <p data-aos="fade-up"> a3model aims to improve the accessibility of Three.js models by offering keyboard navigation for hover and click events, focus indication on meshes/objects, mobile touch events, cursor updates, and roles/descriptions for screen readers. </p>
         <div id="arduino-container">
-        <!-- <img id="arduino" data-aos="fade-right" alt="Arduino metro mini 3d model" src="https://dreamworthie.s3.us-east-2.amazonaws.com/arduino.png"> -->
-        <div id="arduino-source" data-aos="fade-up">
-          <p>Npm Package <a href="https://www.npmjs.com/package/a3model" target="_blank" rel="noopener noreferrer"><button class="btn btn-success">Try it out!</button></a></p>
-          <p>Open Source Code <a href="https://github.com/HilarieSit/a3" target="_blank" rel="noopener noreferrer"><button class="btn btn-success">View on GitHub</button></a></p>
-        </div>
+          <div data-aos="fade-right" id="a3canvas"><canvas class="webgl"></canvas></div>
+          <div id="arduino-source" data-aos="fade-up">
+            <p>Npm Package <a href="https://www.npmjs.com/package/a3model" class="btn btn-success" target="_blank" rel="noopener noreferrer">Try it out!</a></p>
+            <p>Open Source Code <a href="https://github.com/HilarieSit/a3" class="btn btn-success" target="_blank" rel="noopener noreferrer">View on GitHub</a></p>
+          </div>
         </div>
       </div>
     </div>
@@ -32,6 +32,9 @@
 
 <script>
 import HomeCard from './HomeCard.vue'
+import * as THREE from 'three'
+import A3 from 'a3model'
+import 'a3model/src/index.css'
 
 export default {
   components: {
@@ -45,6 +48,79 @@ export default {
         {'id': 2, 'name': 'custom', 'title': 'Fill in Templates', 'description': '', 'url': 'https://picsum.photos/id/1037/200/300'}
       ]
     }
+  },
+  methods: {
+    init: function() {
+        const canvas = document.querySelector('canvas.webgl')
+
+        // Sizes
+        const sizes = {
+            width: 400,
+            height: 400
+        }
+
+        // Renderer
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: canvas,
+            alpha: true
+        })
+        this.renderer.setSize(sizes.width, sizes.height);
+        this.renderer.setClearColor( 0x000000, 0 );
+
+        // A3 
+        const a3canvas = document.querySelector('#a3canvas');
+        this.mya3 = new A3(canvas, this.renderer, a3canvas, sizes);
+
+        // Scene
+        this.scene = new THREE.Scene()
+
+        // Camera 
+        this.camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+        this.camera.position.z = 30
+        this.scene.add(this.camera)
+
+        // Mesh
+        const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
+        const material = new THREE.MeshPhongMaterial( { 
+            color: 0x9862070,
+            specular: 0x050505,
+            shininess: 100
+        } ) 
+        this.mesh = new THREE.Mesh(geometry, material)
+        this.mesh.name = "mesh"
+        this.mesh = this.mya3.createBox(this.mesh, "button")
+        this.scene.add(this.mesh)
+
+        const light = new THREE.AmbientLight(0x404040); // soft white light
+        this.scene.add( light );
+
+        const spotLight = new THREE.SpotLight(0xffffff);
+        spotLight.position.set(100, 100, 100);
+        spotLight.castShadow = true;
+        this.scene.add( spotLight );
+
+        // A3 Click 
+        let funct = this.mya3.functWrapper(changeColor, this.mesh)
+        this.mya3.click(this.mesh.name, funct, 'mesh color changed on click', this.camera)
+
+        function changeColor(child){
+            child.material.color.setHex(Math.random() * 0xffffff);
+        }
+
+        this.mya3.renderEffects(this.camera)
+    },
+    animate: function() {
+        requestAnimationFrame(this.animate);
+        this.mesh.rotation.x += 0.002;
+        this.mesh.rotation.y += 0.002;
+        this.renderer.render(this.scene, this.camera);
+        this.mya3.updateBoxes(this.camera)
+        this.mya3.render(this.scene, this.camera)
+    }
+  },
+  mounted() {
+      this.init();
+      this.animate();
   }
 }
 </script>
@@ -105,7 +181,7 @@ h1{
 
 #arduino-source p{
   color: white;
-  padding: 10px 30px;
+  padding: 0 30px;
 }
 
 #banner{
